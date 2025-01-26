6ddcd8507dbf6ccbdc3352c888bd92a02772dad7
jonesywolf <4johnryanwolf@gmail.com>
2025-01-25 23:34:14 -0500

    Removed TM, renamed Hackathons to Ventures

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -152,7 +152,7 @@ export default function Page() {
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
                 <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-                  Hackathons
+                  Projects
                 </div>
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
                   I like building things
@@ -195,7 +195,7 @@ export default function Page() {
                 I like making money
               </h2>
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                Coming Soon™️
+                Coming Soon...
               </p>
             </div>
           </BlurFade>
